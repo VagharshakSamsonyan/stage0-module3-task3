71eed638046fee45eba3a4b9a987e9238d7d3bdc
Vagharshak Samsonyan <vagosamsonyan@yahoo.com>
2024-03-30 21:42:04 +0400

    5 of 5 tests passed

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -2,6 +2,34 @@ package school.mjc.stage0.module3.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first = 1, second = 10, third = 100;
+
+        // Initial values
+       // System.out.println("Initial values:");
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+        // Reassigning references
+        int linkToFirst = first;
+        int linkToSecond = second;
+        int linkToThird = third;
+
+        // Reassigning values
+        first = 15;
+        second = 6;
+        third = 4;
+
+        // Final values
+       // System.out.println("Final values:");
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
     }
 }
+
+
+
